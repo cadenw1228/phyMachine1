896c5288a57c2e428eef3695a2952f100be179cd
Caden <cadenw1228@gmail.com>
2020-04-29 09:43:08 -0600

    tab updates

--- phy-machine-master/src/app/Main.java
+++ phy-machine-master/src/app/Main.java
@@ -13,6 +13,10 @@ public class Main extends JFrame{
     //static J t1 = new JLabel("idk:");
     static String[] mechLabels = {"Change in distance(meters)","Change in time(seconds)","velocity(m/s)","change in velocity","acceleration((m/2)^2)","final velocity", "inital velocity"};
     static JTextField[] f1 = new JTextField[7];
+
+    static String[] forceLabels = {"force(newtons)","mass(kilograms)","acceleration((m/2)^2)","momentum(kg*m/2)","velocity(m/2)","impulse(kg*m/s)","Change in time(seconds)","kinetic energy(juels)","potential energy (juels)","distance(meters)","Power(watts)","Energy(juels)"};
+    static JTextField[] f2 = new JTextField[12];
+
     static JButton btn = new JButton("check");
     static JButton btn1 = new JButton("mechanics");
     static JButton btn2 = new JButton("forces");
@@ -21,23 +25,23 @@ public class Main extends JFrame{
     static JLabel t2 = new JLabel();
     //electricity elec = new electricity();
     mechanics mech = new mechanics();
-
+    final GridLayout layoutForce = new GridLayout(4,1,10,10);
     public Main() {
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         int height = (int)screenSize.getHeight();
         int width = (int)screenSize.getWidth();
         setSize(width,height);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        final GridLayout layout = new GridLayout(8, 1, 10, 10);
+        final GridLayout layout = new GridLayout(4, 1, 10, 10);
         setLayout(layout);
-        final GridLayout layout4 = new GridLayout(1,4,10,10);
-        row3.setLayout(layout4);
+        final GridLayout layoutBtn = new GridLayout(1,4,10,10);
+        row3.setLayout(layoutBtn);
         row3.add(btn1);
         row3.add(btn2);
         row3.add(btn3);
         row3.add(btn4);
-        final GridLayout layout2 = new GridLayout(4,1,10,10);
-        row.setLayout(layout2);
+        final GridLayout layoutMech = new GridLayout(4,1,10,10);
+        row.setLayout(layoutMech);
         //row.add(t1);
         for (int i = 0; i < 7; i++) {
             f1[i] = new JTextField();
@@ -63,23 +67,49 @@ public class Main extends JFrame{
         final Main frame = new Main();
         //System.out.println("yes");
     }
+
+
     public void checker(){
+
         btn1.addActionListener(new ActionListener()
         {
             public void actionPerformed(ActionEvent e){
                 System.out.println("btn1");
+                row.setVisible(false);
                 row.removeAll();
-                row2.removeAll();
+                for (int i = 0; i < mechLabels.length; i++) {
+                    f1[i] = new JTextField();
+                    JLabel t1 = new JLabel(mechLabels[i]);
+                    row.add(t1);
+                    row.add(f1[i]);
+                }
+                //t2.setText("yes");
+                row.setVisible(true);
                 //row3.removeAll();
+                //t2.setText("yes");
+                
                 
             }
         });
+
         btn2.addActionListener(new ActionListener()
         {
             public void actionPerformed(ActionEvent e){
                 System.out.println("btn2");
+                row.setVisible(false);
+                row.removeAll();
+                row.setLayout(layoutForce);
+                for (int i = 0; i < forceLabels.length; i++) {
+                    f2[i] = new JTextField();
+                    JLabel t1 = new JLabel(forceLabels[i]);
+                    row.add(t1);
+                    row.add(f2[i]);
+                }
+                //t2.setText("yes");
+                row.setVisible(true);
             }
         });
+
         btn3.addActionListener(new ActionListener()
         {
             public void actionPerformed(ActionEvent e){
